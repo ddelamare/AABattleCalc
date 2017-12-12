9b37a45778a55f63fb6a9e448ee16079e9b7b789
Daniel deLamare <daniel.delamare@live.com>
2017-12-11 22:48:16 -0800

    Added IPC local storage persistance

--- AAHelper/VueScripts/NumberStepper.js
+++ AAHelper/VueScripts/NumberStepper.js
@@ -1,11 +1,27 @@
 ﻿Vue.component('number-stepper', {
     template: `
     <div>
-        <button class="btn btn-sm col-xs-2" v-on:click="value -= 1">-</button>
+        <button class="btn btn-sm col-xs-2" v-on:click="internalValue -= 1">-</button>
             <span class="col-xs-8">
-                <input class="form-control" type="text" v-model="value"/>
+                <input class="form-control" type="text" v-model="internalValue"/>
             </span>
-        <button class="btn btn-sm col-xs-2" v-on:click="value += 1">+</button>
+        <button class="btn btn-sm col-xs-2" v-on:click="internalValue += 1">+</button>
     </div>`,
-    props: ['value']
+    props: ['value','id','update'],
+    data: function () {
+        return {
+            internalValue:  0
+        }
+    },
+    created: function () {
+        this.internalValue = this.value;
+    },
+    watch: {
+        internalValue: {
+            handler: function (oldVal, newVal) {
+            if (this.update)
+                this.update(this.id, this.internalValue );
+            }
+        }
+    }
 });
--- AAHelper/VueScripts/TurnOrder.js
+++ AAHelper/VueScripts/TurnOrder.js
@@ -1,12 +1,36 @@
 ﻿Vue.component('turn-order', {
     data() {
         return createTurnState();
+    },
+    methods: {
+        updateVal: function (id, ipcs) {
+            console.log(id, ipcs);
+            localStorage.setItem(buildStorageKey(id), ipcs);
+        },
+        reset: function () {
+            if (confirm("Do you want to wipe out IPC data?"));
+            {
+                for (var i = 0; i < this.countries.length; i++) {
+                    localStorage.removeItem(buildStorageKey(this.countries[i].Name));
+                }
+                location.reload();
+            }
+        }
     }
 });
+  
+function buildStorageKey(id) {
+    return "IPC_" + id;
+}
+
 
 
 function createTurnState() {
     var data = {};
     data.countries = $.extend([], Factions.Countries);// Clone units data
+    for (var i = 0; i < data.countries.length; i++){
+        var ipcVal = localStorage.getItem(buildStorageKey(data.countries[i].Name));
+        if (ipcVal) data.countries[i].IPC = parseInt(ipcVal);
+    }
     return data;
 }
